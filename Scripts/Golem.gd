012b9111f306221af7b1b3a37180c209da699d06
Noir <leroguenoir@gmail.com>
2022-07-09 03:51:43 +0400

    Functions!
    Refactored player code into functions
    Made golem.gd!
    Static typing

--- Scripts/Golem.gd
+++ Scripts/Golem.gd
@@ -1,19 +1,4 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-onready var gay = $AnimationTree.get("parameters/playback")
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	gay.travel("Fall")
-func fallen():
-	gay.travel("Idle")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass
--- Scripts/Knight.gd
+++ Scripts/Knight.gd
@@ -1,77 +1,91 @@
 extends KinematicBody2D
 
-var motion = Vector2()
-var rolling = false
-var attacking = false
-var stamina = 100
+var velocity := Vector2()
+var is_rolling := false
+var is_attacking := false
+var stamina := 100.0
 onready var anim = $AnimationTree.get("parameters/playback")
 
-#Stats
-var speed = 64
-var rollSpeed = 192
-var iframes = 1
-var hp = 3
-var defense = 1
-var attack = 1
+# Stats
+var speed := 64
+var roll_speed := 192
+var iframes := 1
+var hp := 3
+var defense := 1
+var attack := 1
+const STAMINA_NEEDED_FOR_ROLL := 20
 
 
 
 func _physics_process(delta):
+	regen_stamina()
 
-	#Stamina Regen
+	# Movement
+	if !is_rolling:
+		if Input.is_action_pressed("ui_right"):
+			move(true) # Move Right
+		elif Input.is_action_pressed("ui_left"):
+			move(false) # Move Left 
+		else:
+			idle()
+
+	# Roll
+	if Input.is_action_just_pressed("ui_z"):
+		try_to_roll()
+	if is_rolling:
+		move_in_rolling_direction()
+
+	# Attack
+	if Input.is_action_just_pressed("ui_x"):
+		is_attacking = true
+
+	# Equipment
+	$Sword.set_frame($Sprite.get_frame())
+	
+	move_and_slide(velocity)
+	print(is_rolling)
+
+# Stamina Regen
+func regen_stamina() -> void:
 	if stamina < 100:
 		stamina += 0.2
 
-	#Movement
-	if rolling == false:
-		if Input.is_action_pressed("ui_right"):
-			motion.x = speed
-			$Sprite.flip_h = false
-			$Sword.flip_h = false
-			$Camera2D.offset_h = lerp($Camera2D.offset_h, 0, 0.2)
-			if attacking == false:
-				anim.travel("Walk")
-			else:
-				anim.travel("AttackMoving")
-		elif Input.is_action_pressed("ui_left"):
-			motion.x = -speed
-			$Sprite.flip_h = true
-			$Sword.flip_h = true
-			$Camera2D.offset_h = lerp($Camera2D.offset_h, -0.8, 0.2)
-			if attacking == false:
-				anim.travel("Walk")
-			else:
-				anim.travel("AttackMoving")
-		else:
-			motion.x = 0
-			if attacking == false:
-				anim.travel("Idle")
-			else:
-				anim.travel("AttackIdle")
+func move_in_rolling_direction() -> void:
+	if !$Sprite.flip_h:
+		velocity.x = roll_speed
+	else:
+		velocity.x = -roll_speed
 
-	#Roll
-	if Input.is_action_just_pressed("ui_z") and stamina >= 20:
-		rolling = true
-		stamina -= 20
-		attacking = false
-	if rolling == true:
+func try_to_roll() -> void:
+	if stamina >= STAMINA_NEEDED_FOR_ROLL:
+		is_rolling = true
+		stamina -= STAMINA_NEEDED_FOR_ROLL
+		is_attacking = false
 		anim.travel("Roll")
-		if $Sprite.flip_h == false:
-			motion.x = rollSpeed
-		else:
-			motion.x = -rollSpeed
-	
-	#Attack
-	if Input.is_action_just_pressed("ui_x"):
-		attacking = true
 
-	#Equipment
-	$Sword.set_frame($Sprite.get_frame())
+func finish_rolling() -> void:
+	is_rolling = false
+
+func finish_attacking() -> void:
+	is_attacking = false
 	
-	move_and_slide(motion)
-	print(stamina)
-	
-func rollOver():
-	rolling = false
-func attackOver():
-	attacking = false
+func move(move_right: bool) -> void:
+	if move_right:
+		velocity.x = speed
+		$Camera2D.offset_h = lerp($Camera2D.offset_h, 0, 0.2)
+	else:
+		velocity.x = -speed
+		$Camera2D.offset_h = lerp($Camera2D.offset_h, -0.8, 0.2)
+	$Sprite.flip_h = !move_right
+	$Sword.flip_h = !move_right
+	if !is_attacking:
+		anim.travel("Walk")
+	else:
+		anim.travel("AttackMoving")
+
+func idle() -> void:
+	velocity.x = 0
+	if !is_attacking:
+		anim.travel("Idle")
+	else:
+		anim.travel("AttackIdle")
